e493542800158f0d36eabd6b7613f68b3f8e7eee
eastorigin <eastorigin21@gmail.com>
2024-01-08 17:00:57 +0900

    강의 자료

---
+++ 03_Javascript/함수 만들기.js
@@ -0,0 +1,13 @@
+function multifly (num1, num2){
+    return num1*num2;
+}
+console.log(multifly(3,7))
+console.log(multifly(2,2))
+
+const square = function (num){
+    console.log(num**2)
+}
+
+square(4);
+square(11);
+square(5);
