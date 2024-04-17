add9c1337b10b327cb9c9524df6ac947cae6709c
eastorigin <eastorigin21@gmail.com>
2024-04-17 16:04:44 +0900

    강의자료

--- 33_socket.io_react_MVC/client/src/components/Practice1.jsx
+++ 33_socket.io_react_MVC/client/src/components/Practice1.jsx
@@ -6,33 +6,35 @@ const socket = io.connect("http://localhost:8080", { autoConnect: false });
 export default function Practice1() {
   const [fromServerStr, setFromServerStr] = useState("");
   const initSocketConnect = () => {
-    console.log(socket.connected);
     if (!socket.connected) socket.connect();
   };
 
   useEffect(() => {
     initSocketConnect();
+
+    socket.on("hello2", (message) => {
+      setFromServerStr("server: " + message);
+    });
+
+    socket.on("study2", (message) => {
+      setFromServerStr("server: " + message);
+    });
+
+    socket.on("bye2", (message) => {
+      setFromServerStr("server: " + message);
+    });
   }, []);
 
   const hello = () => {
     socket.emit("hello", "hello");
-    socket.on("hello2", (message) => {
-      setFromServerStr("server: " + message);
-    });
   };
 
   const study = () => {
     socket.emit("study", "study");
-    socket.on("study2", (message) => {
-      setFromServerStr("server: " + message);
-    });
   };
 
   const bye = () => {
     socket.emit("bye", "bye");
-    socket.on("bye2", (message) => {
-      setFromServerStr("server: " + message);
-    });
   };
 
   return (
