298031e768b85ecf3738c78bba988e0413a83ed1
eastorigin <eastorigin21@gmail.com>
2024-05-10 17:13:21 +0900

    연습

--- java-practice/src/Main.java
+++ java-practice/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("이름을 입력하세요");
+        String name = scan.next();
+        System.out.println("나이를 입력하세요");
+        int age = scan.nextInt();
+        scan.close();
+
+        System.out.println("안녕하세요!" + name + "님("+age+"세)");
     }
 }
